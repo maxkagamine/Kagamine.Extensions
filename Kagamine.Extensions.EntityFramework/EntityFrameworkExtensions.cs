db8e278886fa03f9beb0dbfbadb9e682033ef3ed
Max Kagamine <max@kgmn.jp>
2025-12-01 21:02:10 +0900

    Dropped support for .NET 8; updated dependencies

--- Kagamine.Extensions.EntityFramework/EntityFrameworkExtensions.cs
+++ Kagamine.Extensions.EntityFramework/EntityFrameworkExtensions.cs
@@ -8,34 +8,6 @@ namespace Kagamine.Extensions.EntityFramework;
 
 public static class EntityFrameworkExtensions
 {
-#if NET8_0
-    /// <summary>
-    /// Asynchronously creates a <see cref="HashSet{T}"/> from an <see cref="IQueryable{T}"/> by enumerating it
-    /// asynchronously.
-    /// </summary>
-    /// <typeparam name="TSource">The type of the elements of <paramref name="source"/>.</typeparam>
-    /// <param name="source">A <see cref="IQueryable{T}"/> from which to create a set.</param>
-    /// <param name="comparer">The <see cref="IEqualityComparer{T}"/> implementation to use when comparing values in the
-    /// set, or <see langword="null"/> to use the default <see cref="IEqualityComparer{T}"/> implementation for the set
-    /// type.</param>
-    /// <param name="cancellationToken">A <see cref="CancellationToken"/> to observe while waiting for the task to
-    /// complete.</param>
-    public static async Task<HashSet<TSource>> ToHashSetAsync<TSource>(this IQueryable<TSource> source, IEqualityComparer<TSource>? comparer, CancellationToken cancellationToken = default)
-    {
-        var set = new HashSet<TSource>(comparer);
-        await foreach (var element in source.AsAsyncEnumerable().WithCancellation(cancellationToken))
-        {
-            set.Add(element);
-        }
-
-        return set;
-    }
-
-    /// <inheritdoc cref="ToHashSetAsync{TSource}(IQueryable{TSource}, IEqualityComparer{TSource}?, CancellationToken)"/>
-    public static Task<HashSet<TSource>> ToHashSetAsync<TSource>(this IQueryable<TSource> source, CancellationToken cancellationToken = default)
-        => ToHashSetAsync(source, null, cancellationToken);
-#endif
-
     /// <summary>
     /// Sets the values and navigation properties of this entity by copying from the given object.
     /// </summary>
